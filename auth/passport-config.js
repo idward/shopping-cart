a547fb69ad80455a6993d1e34cae9f294e254af3
idwardluo <apple@idwardluo.local>
2017-05-09 05:24:21 +0800

    update shopping-cart

--- auth/passport-config.js
+++ auth/passport-config.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
 
-//local strategy for passport
+//local strategy for signup
 passport.use('local.signup', new LocalStrategy({
         usernameField: 'email',
         passwordField: 'password',
@@ -10,8 +10,8 @@ passport.use('local.signup', new LocalStrategy({
     },
     function (req, email, password, done) {
         //验证提交字段
-        req.checkBody('email','Email is required').notEmpty();
-        req.checkBody('password','Password is required').notEmpty();
+        req.checkBody('email', 'Email is required').notEmpty();
+        req.checkBody('password', 'Password is required').notEmpty();
         req.checkBody('email', 'Invalid Eamil').isEmail();
         req.checkBody('password', 'Invalid Password').isLength({min: 4});
         var errors = req.validationErrors();
@@ -45,6 +45,40 @@ passport.use('local.signup', new LocalStrategy({
             });
         });
     }));
+
+//local strategy for signin
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function (req, email, password, done) {
+    //提交字段验证
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Invalid Email').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        })
+        return done(null, false, req.flash('error', messages));
+    }
+    //查找用户是否存在
+    User.findOne({email: email}, function (err, user) {
+        if (err) {
+            return done(null, false, {message: 'Database error.'});
+        }
+        if (!user) {
+            return done(null, false, {message: 'No user found.'});
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: 'Password doesn\'t match'});
+        }
+        return done(null, user);
+    });
+
+}));
 
 //user put into session
 passport.serializeUser(function (user, done) {
--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,20 @@ router.get('/signup', function (req, res, next) {
 
 router.post('/signup', passport.authenticate('local.signup', {
     failureRedirect: '/users/signup',
+    badRequestMessage: 'email or password is not empty',
+    failureFlash: true
+}), function (req, res, next) {
+    res.redirect('/users/signin');
+});
+
+router.get('/signin', function (req, res, next) {
+    var messages = req.flash('error');
+    res.render('user/signin',{csrfToken:req.csrfToken(),errorMessages:messages});
+});
+
+router.post('/signin', passport.authenticate('local.signin', {
+    failureRedirect: '/users/signin',
+    badRequestMessage: 'email or password is not empty',
     failureFlash: true
 }), function (req, res, next) {
     res.redirect('/users/profile');
